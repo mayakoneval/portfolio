849722e5ddf262b93b13368dd8ce1be17d9cff23
mkoneval <mkoneval@mit.edu>
2019-07-20 15:49:09 -0700

    init gatsby app, add goat photos and descriptions

---
+++ src/components/layout.js
@@ -0,0 +1,42 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+
+import Header from "./header"
+import "./layout.css"
+import ProjectSection from "./projectSection";
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <ProjectSection 
+      bgColor="white" 
+      title="This is a Project Name" 
+      description="Test here is a description for this project. I did this this way because I wanted it to be this way and not another way and thats okay for that thing to happen the way that it does" 
+      imageUrl="https://i.ytimg.com/vi/cEJy2q27hVk/maxresdefault.jpg"/>
+      <ProjectSection 
+      bgColor="black" 
+      title="This is a Another Project Name" 
+      alignRight={true} 
+      description="Test here is a description for this project. I did this this way because I wanted it to be this way and not another way and thats okay for that thing to happen the way that it does" 
+      imageUrl="https://i.ytimg.com/vi/cEJy2q27hVk/maxresdefault.jpg"/>
+      <ProjectSection 
+      bgColor="#ec2431" 
+      title="This is a Third Project Name" 
+      description="Test here is a description for this project. I did this this way because I wanted it to be this way and not another way and thats okay for that thing to happen the way that it does"  
+      imageUrl="https://i.ytimg.com/vi/cEJy2q27hVk/maxresdefault.jpg"/>
+    </div>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
